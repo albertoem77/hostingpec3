645a58eca38f20fd912f0f60451fe1a7a8f3028d
Alberto <85483883+albertoem77@users.noreply.github.com>
2023-12-27 16:10:27 +0100

    files for web page

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// JavaScript para manejar funcionalidades adicionales como scrolling
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Aquí se puede añadir código para manejar eventos como el scrolling
+});
